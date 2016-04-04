c2dfbdfd8a329f031f21a2e9ba8f6801b42974ae
kuwkuw <kuw-kuw@mail.ru>
2016-04-04 19:53:10 +0300

    pull request

--- app/app.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-var GameComponent = require('./gameComponent.js');
-
-var game = new GameComponent({element: document.querySelector('[data-component="game"]')});
-
-document.querySelector('[data-selector="restart"]').addEventListener('click', game.restart.bind(game));
-
-
